97f07e55c659ac4213500be1342b412d905e3d10
Eric Rumble <eric.rumble@sage.com>
2016-07-29 13:45:38 -0400

    only sync when it's safe
    don't retry forever
    
    retry curl on transient errors
    
    clean tmp dir for each retry

--- update_repo.sh
+++ update_repo.sh
@@ -1,17 +1,46 @@
-#!/bin/sh -eux
+#!/bin/sh -eu
 
-repo_dir=$HOME/$S3_BUCKET
+repo_dir=`mktemp -d -t`
 s3_url=s3://$S3_BUCKET
+retry_count=0
 
-# create repo directory if it doesn't exist
-if [ ! -d "$repo_dir" ]; then
-  mkdir -pv $repo_dir
-fi
+function cleanup {
+  # clean up after ourselves
+  rm -rf $repo_dir
+}
+trap cleanup EXIT
 
-# pull down repo, run createrepo, push it back up
-aws s3 sync $s3_url $repo_dir
-echo `find $repo_dir -mindepth 2 -maxdepth 2 -type d` | xargs -n 1 -P 8 createrepo --update --deltas
-aws s3 sync $repo_dir $s3_url --delete --exclude "*.rpm"
+function gen_metadata {
+  # make sure our tmp directory is clean
+  rm -rf $repo_dir/*
 
-# clean up after ourselves
-rm -rf $repo_dir
+  # pull down repo and generate metadata, run create repo in parallel
+  aws s3 sync $s3_url $repo_dir --delete
+  echo `find $repo_dir -mindepth 2 -maxdepth 2 -type d` | xargs -n 1 -P 8 createrepo --update --deltas
+
+  # Do the uploady stuff
+  validate_and_sync
+}
+
+function validate_and_sync {
+  # compare the local copy of version with the remote, so we know if the repo has been updated since we started
+  local version=`curl --retry 3 --silent http://$S3_BUCKET.s3-website-$REGION.amazonaws.com/version`
+
+  if [ "$version" == `cat $repo_dir/version` ]; then
+    # generate new version number and sync the repo
+    echo `uuidgen` > $repo_dir/version
+    aws s3 sync $repo_dir $s3_url --delete --exclude "*.rpm"
+  elif (( $retry_count < 3 )); then
+    # start over
+    let retry_count+=1
+    gen_metadata
+  else
+    # give up
+    echo "could not validate and sync"
+    exit 1
+  fi
+}
+
+# ENTRY POINT!!!
+gen_metadata
+
