416dfb7f19dea7fc56f9b478b8e1a95d2e1fee78
Eric Rumble <eric.rumble@sage.com>
2016-07-29 19:04:03 -0400

    update task and lambda defs to be more flexible

--- lambda_function.js
+++ lambda_function.js
@@ -2,17 +2,34 @@ var aws = require('aws-sdk');
 var ecs = new aws.ECS({apiVersion: '2014-11-13'});
 
 exports.handler = function(event, context) {
-  console.log('Recieved event:');
-  console.log(JSON.stringify(event, null, ' '));
+    console.log('Recieved event:');
+    console.log(JSON.stringify(event, null, ' '));
 
-  var params = {
-    taskDefinition: 's3-repo-sync',
-    cluster: 'default',
-    count: 1,
-    startedBy: 'lambda'
-  };
-  ecs.runTask(params, function(err, data) {
-    if (err) console.log(err, err.stack);
-    else     console.log(data);
-  });
+    var params = {
+        taskDefinition: 'arn:aws:ecs:<aws_region>:<aws_account>:task-definition/s3-repo-sync',
+        cluster: 'default',
+        count: 1,
+        overrides: {
+            containerOverrides: [
+                {
+                    name: 's3-repo-sync',
+                    environment: [
+                        {
+                            name: 'S3_BUCKET',
+                            value: event.Records[0].s3.bucket.name
+                        },
+                        {
+                            name: 'REGION',
+                            value: event.Records[0].awsRegion
+                        }
+                    ]
+                }
+            ]
+        },
+        startedBy: 'lambda'
+    };
+    ecs.runTask(params, function(err, data) {
+        if (err) console.warn(err, err.stack);
+        else     console.info(data);
+    });
 };
